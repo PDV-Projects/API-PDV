35a5acd87c5a0bcb9ea2307a9eb8bebeafb38b7d
Gustavo Cavalcanti <55290097+tfhgzeo@users.noreply.github.com>
2025-07-12 18:01:17 -0300

    feat(business): add business registration dto validation

--- src/modules/business/dto/business-registration.dto.ts
+++ src/modules/business/dto/business-registration.dto.ts
@@ -1,1 +1,15 @@
-export class BusinessRegistratiosDto {}
+import { IsBoolean, IsEmail, IsNotEmpty, IsNumber, IsString, MinLength } from 'class-validator';
+
+export class BusinessRegistratiosDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsString()
+  @IsNotEmpty()
+  owner: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  cnpj: number
+}
