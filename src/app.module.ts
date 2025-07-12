0c4eb18486f6c319b95689b02ee37345800817b7
Gustavo Cavalcanti <55290097+tfhgzeo@users.noreply.github.com>
2025-07-12 17:40:29 -0300

    feat(common): add global http exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,24 @@ import { CashRegisterModule } from '@modules/cash-register/cash-register.module'
 import { MongooseModule } from '@nestjs/mongoose';
 import { BusinessService } from '@modules/business/business.service';
 import { BusinessModule } from '@modules/business/business.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from '@common/filter/http-expection.filter';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`),
-    AuthModule, UsersModule, CartModule, CashRegisterModule, BusinessModule],
+    AuthModule, 
+    UsersModule, 
+    CartModule, 
+    CashRegisterModule, 
+    BusinessModule],
   controllers: [AppController],
-  providers: [AppService, BusinessService],
+  providers: [AppService, 
+    BusinessService, 
+    { 
+      provide: APP_FILTER, 
+      useClass: HttpExceptionFilter 
+    }
+  ],
 })
 export class AppModule {}
