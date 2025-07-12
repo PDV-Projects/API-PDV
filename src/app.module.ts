368024e0c6b65667300c55a06851e2a7d868b0e8
Gustavo Cavalcanti <55290097+tfhgzeo@users.noreply.github.com>
2025-07-12 16:04:37 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { UsersModule } from '@modules/users/users.module';
 import { CartModule } from '@modules/cart/cart.module';
 import { CashRegisterModule } from '@modules/cash-register/cash-register.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BusinessService } from '@modules/business/business.service';
+import { BusinessModule } from '@modules/business/business.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(`mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`),
-    AuthModule, UsersModule, CartModule, CashRegisterModule],
+    AuthModule, UsersModule, CartModule, CashRegisterModule, BusinessModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BusinessService],
 })
 export class AppModule {}
